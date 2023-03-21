15fcb86b4bdc3863d3901bb5fed1b55f4cb9b3fb
sammykingx <hellosammy@sammykingx.com.ng>
2023-03-21 02:17:34 +0000

    completed task 9

---
+++ 0x00-ES6_basic/9-main.js
@@ -0,0 +1,6 @@
+import getFullBudgetObject from './9-getFullBudget';
+
+const fullBudget = getFullBudgetObject(20, 50, 10);
+
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));
