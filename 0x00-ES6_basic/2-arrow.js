8b84cbb396e419694b536afd90da314718074dc5
sammykingx <hellosammy@sammykingx.com.ng>
2023-03-21 00:20:03 +0000

    completed task 2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,6 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => [...self.sanFranciscoNeighborhoods, newNeighborhood];
+}
