5f507377a22e4e1c2352e659fd8a9c81da2635d7
sammykingx <hellosammy@sammykingx.com.ng>
2023-03-21 00:27:14 +0000

    fiexed issue

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,5 +2,8 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = (newNeighborhood) => [...self.sanFranciscoNeighborhoods, newNeighborhood];
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
