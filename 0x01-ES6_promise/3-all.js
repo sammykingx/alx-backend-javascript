5a227d60a835985e60506b303e21a40f9ba59d5e
sammykingx <hellosammy@sammykingx.com.ng>
2023-03-24 01:17:05 +0000

    removed the variable outputdata

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const data= [uploadPhoto(), createUser()];
+  const data = [uploadPhoto(), createUser()];
 
-  const outputData = Promise.all(data)
+  Promise.all(data)
     .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
-    () => console.log('Signup system offline'));
+      () => console.log('Signup system offline'));
 }
