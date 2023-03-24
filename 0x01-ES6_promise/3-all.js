b06acca7dff94aa64c7e0615c2e92a8f581c8140
sammykingx <hellosammy@sammykingx.com.ng>
2023-03-24 01:27:40 +0000

    i attcached the return statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const data = [uploadPhoto(), createUser()];
 
-  Promise.all(data)
+  return Promise.all(data)
     .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
       () => console.log('Signup system offline'));
 }
