12926cd03bae7f1c0a03a0d3125f594e52dd8cb2
sammykingx <hellosammy@sammykingx.com.ng>
2023-03-24 01:09:52 +0000

    tweaked the code structure

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const x = uploadPhoto();
-  const y = createUser();
-  return Promise.all([x, y])
+  const data= [uploadPhoto(), createUser()];
+
+  const outputData = Promise.all(data)
     .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
-      () => console.log('Signup system offline'));
+    () => console.log('Signup system offline'));
 }
